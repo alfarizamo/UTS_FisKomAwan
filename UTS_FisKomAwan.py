58f4b8ec88485437a4afe9936893f3c49f29d334
alfarizamo <160070893+alfarizamo@users.noreply.github.com>
2024-10-24 09:12:02 +0700

    Update UTS_FisKomAwan.py

--- UTS_FisKomAwan.py
+++ UTS_FisKomAwan.py
@@ -2,45 +2,38 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Judul
 st.title("FISIKA KOMPUTASI AWAN")
 st.header("RIZKY SETYAWAN")
 st.header("220322601527")
 
-# Tombol untuk menghasilkan data acak
+# tombol data untuk mengacak data
 if st.button('Data'):
-    # Buat data acak untuk plot lingkaran
     num_points = 100
     theta = 2 * np.pi * np.random.rand(num_points)
     r = np.sqrt(np.random.rand(num_points))
-
     x = r * np.cos(theta)
     y = r * np.sin(theta)
+    sizes = 1000 * np.random.rand(num_points)
+    colors = np.random.rand(num_points)
 
-    sizes = 1000 * np.random.rand(num_points)  # Ukuran lingkaran acak
-    colors = np.random.rand(num_points)  # Warna acak
+    fig, ax = plt.subplots(figsize=(8, 8))
 
-    # Perlebar plot dengan ukuran lebih besar
-    fig, ax = plt.subplots(figsize=(8, 8))  # Tambah figsize untuk memperlebar plot
-
-    # Plot lingkaran acak dengan warna dan ukuran yang berbeda
+    # data/warna acak
     scatter = ax.scatter(x, y, s=sizes, c=colors, alpha=0.6, cmap='viridis')
 
-    # Tambahkan garis dari titik pusat (0, 0) ke setiap data acak
+    # garis
     for i in range(num_points):
         ax.plot([0, x[i]], [0, y[i]], color=plt.cm.viridis(colors[i]), alpha=0.5)
 
-    # Atur tampilan lingkaran dengan radius 1
+    # lingkaran
     circle = plt.Circle((0, 0), 1, color='red', fill=False)
     ax.add_artist(circle)
-
-    # Atur batas tampilan agar lebih luas dan lingkaran terlihat jelas
     ax.set_xlim([-1.0, 1.0])
     ax.set_ylim([-1.0, 1.0])
     ax.set_aspect('equal', 'box')
 
-    # Tambahkan grid untuk mempermudah penglihatan
+    # grid
     ax.grid(True)
 
-    # Tampilkan plot
+    # menampilkan plot
     st.pyplot(fig)
