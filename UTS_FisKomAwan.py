ced7ecaa407e58c567fc0138fd3036627568d531
alfarizamo <160070893+alfarizamo@users.noreply.github.com>
2024-10-24 09:05:09 +0700

    Update UTS_FisKomAwan.py

--- UTS_FisKomAwan.py
+++ UTS_FisKomAwan.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 # Judul
 st.title("FISIKA KOMPUTASI AWAN")
-st.header("NAMA")
+st.header("RIZKY SETYAWAN")
+st.header("220322601527")
 
 # Tombol untuk menghasilkan data acak
 if st.button('Data'):
@@ -19,16 +20,27 @@ if st.button('Data'):
     sizes = 1000 * np.random.rand(num_points)  # Ukuran lingkaran acak
     colors = np.random.rand(num_points)  # Warna acak
 
-    # Plot
-    fig, ax = plt.subplots()
-    ax.scatter(x, y, s=sizes, c=colors, alpha=0.6, cmap='viridis')
+    # Perlebar plot dengan ukuran lebih besar
+    fig, ax = plt.subplots(figsize=(8, 8))  # Tambah figsize untuk memperlebar plot
+
+    # Plot lingkaran acak dengan warna dan ukuran yang berbeda
+    scatter = ax.scatter(x, y, s=sizes, c=colors, alpha=0.6, cmap='viridis')
+
+    # Tambahkan garis dari titik pusat (0, 0) ke setiap data acak
+    for i in range(num_points):
+        ax.plot([0, x[i]], [0, y[i]], color=plt.cm.viridis(colors[i]), alpha=0.5)
 
     # Atur tampilan lingkaran dengan radius 1
     circle = plt.Circle((0, 0), 1, color='black', fill=False)
     ax.add_artist(circle)
 
-    ax.set_xlim([-1.2, 1.2])
-    ax.set_ylim([-1.2, 1.2])
+    # Atur batas tampilan agar lebih luas dan lingkaran terlihat jelas
+    ax.set_xlim([-1.5, 1.5])
+    ax.set_ylim([-1.5, 1.5])
     ax.set_aspect('equal', 'box')
 
+    # Tambahkan grid untuk mempermudah penglihatan
+    ax.grid(True)
+
+    # Tampilkan plot
     st.pyplot(fig)
